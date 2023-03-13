4e0ca3b02123dcc8a7ec5962cfd03aeac94c6b25
= <=>
2023-03-13 04:45:04 +0100

    ...

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,37 +1,41 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from './Button';
+import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
-const meta: Meta<typeof Button> = {
-  title: 'Ensome/Components/Button',
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
   component: Button,
-  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Button',
 };
 
-export default meta;
-type Story = StoryObj<typeof Button>;
-
-// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
-export const Enabled: Story = {
-  args: {
-    
-  },
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
 };
 
-export const Hovered: Story = {
-  args: {
-  },
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
 };
 
-export const Pressed: Story = {
-  args: {
-   
-  },
-};
-
-export const Disabled: Story = {
-  args: {
- 
-  },
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
 };
