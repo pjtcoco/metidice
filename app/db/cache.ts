af51583d7b7094144907b7ebed920e297833d534
= <=>
2023-03-13 13:13:29 +0100

    cleaned

--- app/db/cache.ts
+++
@@ -1,6 +0,0 @@
-import { cache } from 'react';
-
-export const getUser = cache(async (id: string,db:any) => {
-  const user = await db.user.findUnique({ id });
-  return user;
-});
--- components/icons/props.ts
+++
@@ -1,4 +0,0 @@
-import { ColorProps } from "@/stories/Colors";
-export interface IconProps extends ColorProps{
-  size?:"small"|"medium"|"large";
-}
