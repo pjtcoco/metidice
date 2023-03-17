13ff84486a3725a2dcbf774fb61cb7de9a7b1e13
= <=>
2023-03-17 12:26:11 +0100

    Completed Review cards

--- components/cards/ReviewCard.tsx
+++ components/cards/ReviewCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HeadlineText from "../text/HeadlineText";
 import ParagraphText from "../text/ParagraphText";
-import photo from 'public/images/rcard.png'
+import photo from "public/images/rcard.png";
 import Image from "next/image";
 
 interface ReviewCardProps {
@@ -9,14 +9,15 @@ interface ReviewCardProps {
   name: string;
   imageSrc?: string;
   type: "variant1" | "variant2";
+  children?: React.ReactNode;
 }
-const ReviewCard: React.FC<ReviewCardProps> = ({ imageSrc,type, title, name }) => {
+const ReviewCard: React.FC<ReviewCardProps> = ({ children, imageSrc, type, title, name }) => {
   return (
     <section className={`ensome__reviewcard__${type}`}>
-      <Image src={imageSrc??photo} alt="card_image" id="rcard_image" width={100} height={100}/>
+      <Image src={imageSrc ?? photo} alt="card_image" id="rcard_image" width={100} height={100} />
       <ParagraphText variant={"p2-r"}>
-        “Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate
-        velit esse quam nihil molestiae consequatur, vel illum.”
+        {children ??
+          "“Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum.”"}
       </ParagraphText>
       <div className={`ensome__reviewcard__${type}_info`}>
         <HeadlineText variant={"h6"}>{name ?? "Alex Bern"}</HeadlineText>
